4fc2075d0e911b89694087794193dfaed2cce3b2
Akula <6536430@gmail.com>
2023-06-12 11:06:08 +0300

    fix error

--- components/feature.tsx
+++ components/feature.tsx
@@ -6,7 +6,7 @@ const Feature = () => {
             <div className="text">
                 <h3 className="title title-section ">Feature</h3>
                 <ul className='list'>
-                    <li>-I'm using next.js for the first time for development.</li>
+                    <li>-I am using next.js for the first time for development.</li>
                     <li>-All of arts on this page was created by me.</li>
                     <li>-I had never used typescript before either.</li>
                 </ul>
